54208b36937666e39b280b1ba1a5f7f53dd21306
Jose Segura <jossegura2023@u.northwestern.edu>
2023-06-27 19:06:05 -0500

    added code for frontend code

---
+++ public/js/delete-post.js
@@ -0,0 +1,17 @@
+const delButtonHandler = async (event) => {
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to delete post');
+    }
+};
+
+document.querySelector('.delete-post-btn').addEventListener('click', delButtonHandler);
